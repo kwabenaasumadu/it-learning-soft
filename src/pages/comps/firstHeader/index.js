459b612220b28a92308f54b2c003da372a6a16d0
Kwabena <kasumadu@tapnhop.com>
2024-02-17 15:36:36 -0800

    first commit

---
+++ src/pages/comps/firstHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../../../styles/firstheader.module.css'
+
+function Index() {
+  return (
+    <>
+      <div className={styles.container}>
+
+      </div>
+    </>
+  )
+}
+
+export default Index
