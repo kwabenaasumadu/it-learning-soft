6484b87ba4771de326e3c7122665d7f727e46d82
Kwabena <kasumadu@tapnhop.com>
2024-02-24 14:52:00 -0800

    modifying

--- src/pages/comps/courses/python/index.js
+++ src/pages/comps/courses/python/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
 import styles from "../../../../styles/python.module.css";
 import { db } from "../../../../../firebase.config";
 import { push, ref, get, set } from "firebase/database";
@@ -91,12 +90,10 @@ function Index() {
                 <h1>{data.Title || "Default Title"}</h1>
               </div>
 
-              
-
               <div
                 className={`${styles.itemDescription} ${styles.pythonCodeContainer}`}
               >
-                <SyntaxHighlighter language="python" style={okaidia}>
+                <SyntaxHighlighter language="python">
                   {data.Body}
                 </SyntaxHighlighter>
               </div>
